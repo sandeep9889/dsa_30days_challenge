3087e9e2112c11b05bf4e40d7f648032ff174206
sandeep <sandeepchauhan9630228313@gmail.com>
2021-12-20 19:48:22 +0530

    basic cpp

---
+++ basic/02.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+     int a1 ='a';
+     cout<<a1<<endl;
+
+     int a =23;
+     cout<<a+22<<endl;
+     char b ='B';
+     cout<<b<<endl;
+     bool b1 = true;
+     cout<<b1<<endl;
+     int size = sizeof(a);
+     cout<< "size of a:" <<size<< endl;
+};
